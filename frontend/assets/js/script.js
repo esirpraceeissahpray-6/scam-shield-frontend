369a14623f54bdfe6e1ba6778d15338545621326
esirpraceeissahpray-6 <esirpraceeissahpray@gmail.com>
2026-07-17 11:45:00 +0300

    Load navbar HTML dynamically using fetch

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -1,3 +1,8 @@
+fetch("../components/navbar.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("navbar").innerHTML = data;
+    });
 // Scam Shield Intelligence Engine (Frontend Simulation Layer)
 
 // ---------- LINK SCANNER ----------
